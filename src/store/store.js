879232ce446cb104a73f6ea54fc06ea96902c7df
Serdiuk-Maksym <serdyukmaksimaleksandrovich@gmail.com>
2023-12-30 22:08:27 +0100

    addPersist

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from './contactSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-export default configureStore({
+const persistConfig = {
+  key: 'root',
+  storage,
+  // Optionally, you can whitelist specific reducers to be persisted
+  whitelist: ['contacts'],
+  blacklist: ['register'],
+};
+
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
+
+const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistedReducer,
+    // Add other reducers if needed
   },
 });
+
+const persistor = persistStore(store);
+
+export { store, persistor };
