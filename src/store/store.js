ac52cf6d77786a25d1283a9d036fa9539a295d04
Serdiuk-Maksym <serdyukmaksimaleksandrovich@gmail.com>
2024-01-03 01:08:25 +0100

    fix

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from './contactSlice';
 import { persistStore, persistReducer } from 'redux-persist';
-import createTransform from 'redux-persist-transform-immutable';
 import storage from 'redux-persist/lib/storage';
-
-const immutableTransform = createTransform(
-  inboundState => inboundState,
-
-  outboundState => outboundState,
-
-  { whitelist: ['contacts'], blacklist: ['register'] }
-);
 
 const persistConfig = {
   key: 'root',
   storage,
-  transforms: [immutableTransform],
   whitelist: ['contacts'],
 };
 
